6d63973764a59ca2cc8b51ceb194260841f688d2
ashok450270 <119446086+ashok450270@users.noreply.github.com>
2022-11-30 00:05:05 +0530

    mobile

---
+++ Mobile/Mobile.java
@@ -0,0 +1,34 @@
+package Mobile;
+
+
+
+public class Mobile {
+	
+
+	String mobileBrandName="onePlus";
+	
+	char mobileLogo='1';
+	short noOfMobilePiece=1;
+	int modelNumber=9;
+	long mobileImeiNumber=145675569L;
+	float mobilePrice=35000.76;
+	boolean isDamaged=false;
+	
+	public static void main(String[] args) {
+		
+		Mobile Mobile=new Mobile();
+		System.out.println(Mobile.mobileBrandName);
+		System.out.println(Mobile.mobileImeiNumber);
+		System.out.println(Mobile.mobileLogo);
+		System.out.println(Mobile.mobilePrice);
+		System.out.println(Mobile.modelNumber);
+		System.out.println(Mobile.isDamaged);
+		System.out.println(Mobile.noOfMobilePiece);
+     
+		
+		      
+
+	
+	}		
+	}
+     
